4027c6642b4da1324250ccf05de014f65a281815
1zumisawashun <shun.1zumisawa@gmail.com>
2023-06-29 00:27:20 +0900

    Update "Find Articles" button text to "Find Articles!!"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home = () => {
               <Link href="/articles" legacyBehavior>
                 <a className="group inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 p-0.5 font-medium text-gray-900 hover:text-white focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800">
                   <span className="rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
-                    Find Articles
+                    Find Articles!!
                   </span>
                 </a>
               </Link>
