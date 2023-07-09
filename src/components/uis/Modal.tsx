a6efe32dd30e357d61876a70cf49d7b20ec43681
1zumisawashun <shun.1zumisawa@gmail.com>
2023-07-09 16:55:13 +0900

    fix modal layout

--- src/components/uis/Modal.tsx
+++ src/components/uis/Modal.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 type ModalProps = {
   header?: JSX.Element | string
   body?: JSX.Element | string
@@ -12,7 +10,6 @@ export const Modal: React.FC<ModalProps> = ({
   footer,
   close
 }) => {
-  const [showModal, setShowModal] = React.useState(false)
   return (
     <>
       <div
@@ -20,21 +17,12 @@ export const Modal: React.FC<ModalProps> = ({
         onClick={close}
         aria-hidden="true"
       >
-        <div className="relative mx-auto my-6 w-auto max-w-3xl">
+        <div className="relative mx-auto my-6 w-6/12">
           {/*content*/}
           <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
             {/*header*/}
             <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
-              <h3 className="text-3xl font-semibold">{header}</h3>
-              <button
-                type="button"
-                className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none text-black outline-none focus:outline-none"
-                onClick={close}
-              >
-                <span className="block h-6 w-6 bg-transparent text-2xl text-black opacity-5 outline-none focus:outline-none">
-                  ×
-                </span>
-              </button>
+              <h3 className="text-2xl font-semibold">{header}</h3>
             </div>
             {/*body*/}
             <div className="relative flex-auto p-6">
