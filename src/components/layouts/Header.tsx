df87097a4277a1840b796544e974aefee2d5db68
1zumisawashun <shun.1zumisawa@gmail.com>
2023-07-30 18:35:13 +0900

    fix-header-layout

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,6 +1,7 @@
 import { signIn, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { BaseButton } from '@/components/uis'
+import styles from '@/styles/components/header.module.scss'
 
 export const Header = () => {
   const { data: session, status } = useSession()
@@ -33,27 +34,25 @@ export const Header = () => {
     }
   ] as const
 
+  const filteredItems = items.filter((item) => !item.isHidden)
+
+  /* eslint-disable react/no-array-index-key */
   return (
-    <div className="bg-gray-900">
-      <div className="container mx-auto flex max-w-4xl items-center px-2 py-5">
-        <div className="mx-auto flex w-full flex-wrap items-center">
-          <div className="flex w-full justify-center font-extrabold text-white lg:w-1/2 lg:justify-start">
-            <Link
-              href="/"
-              className="text-2xl text-gray-900 no-underline hover:text-gray-900 hover:no-underline"
-            >
-              📚 &nbsp; <span className=" text-gray-200">BooxMix</span>
-            </Link>
-          </div>
-          <div className="flex w-full content-center justify-between pt-2 lg:w-1/2 lg:justify-end lg:pt-0">
-            <ul className="flex flex-1 items-center justify-center lg:flex-none">
-              {items.map((item) => (
-                <li className="px-4 py-1 text-white no-underline">
-                  <BaseButton {...item}>{item.name}</BaseButton>
-                </li>
-              ))}
-            </ul>
-          </div>
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Link href="/" className={styles.logoLink}>
+            📚 &nbsp; <span className={styles.logoText}>BooxMix</span>
+          </Link>
+        </div>
+        <div className={styles.menu}>
+          <ul className={styles.menuList}>
+            {filteredItems.map((item, index) => (
+              <li key={index} className={styles.menuItem}>
+                <BaseButton {...item}>{item.name}</BaseButton>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
--- src/components/uis/BaseButton.tsx
+++ src/components/uis/BaseButton.tsx
@@ -1,42 +1,28 @@
 import Link, { LinkProps } from 'next/link'
-import { ReactNode, ComponentPropsWithoutRef, ElementType } from 'react'
+import { ReactNode, ComponentProps } from 'react'
 
 type Element<T extends 'button' | 'next-link'> = T extends 'button'
-  ? ElementType<'button'>
+  ? ComponentProps<'button'>
   : LinkProps
 
 type Props<T extends 'button' | 'next-link'> = {
-  tag?: Element<T>
-} & Omit<ComponentPropsWithoutRef<'button'> | LinkProps, 'tag'>
+  tag: T
+  children: ReactNode
+} & Element<T>
 
 type Type = 'button' | 'next-link'
 
-type BaseButtonProps = (
-  | {
-      tag: 'next-link'
-      href: LinkProps['href']
-      onClick?: never
-    }
-  | {
-      tag: 'button'
-      onClick: () => void
-      href?: never
-    }
-) & { children: ReactNode }
-
-export const BaseButton: React.FC<BaseButtonProps> = ({ tag, ...props }) => {
-  if (tag === 'next-link' && props.href) {
-    // `as`が'a'の場合はリンク（<a>要素）としてレンダリング
-    return (
-      <Link {...props} href={props.href}>
-        {props.children}
-      </Link>
-    )
+/* eslint-disable */
+export const BaseButton = <T extends Type = 'button'>({
+  tag,
+  children,
+  ...props
+}: Props<T>) => {
+  // FIXME:型ガードするしかない？
+  if (tag === 'next-link') {
+    const copiedProps = props as unknown as Props<'next-link'>
+    return <Link {...copiedProps}>{children}</Link>
   }
-  // `as`が'button'の場合はボタン（<button>要素）としてレンダリング
-  return (
-    <button type="button" onClick={props.onClick} {...props}>
-      {props.children}
-    </button>
-  )
+  const copiedProps = props as unknown as Props<'button'>
+  return <button {...copiedProps}>{children}</button>
 }
